b2c94bce4d6d843fa0b8cae2f1b0b516fee24258
Aysha1993 <ayshuash95@gmail.com>
2025-05-23 11:26:51 +0000

    Update Streamlit boxing app

--- app.py
+++ app.py
@@ -303,7 +303,7 @@ def draw_annotations(frame, keypoints, punches, postures, gloves, h, w):
         kp = np.array(kp_raw).reshape(-1, 3).tolist()
         kp_norm = [[y / h, x / w, s] for y, x, s in kp]
 
-        if not is_likely_coach(kp_norm):
+        if not is_likely_coach(kp):
             avg_conf = np.mean([p[2] for p in kp_norm])
             ys = [p[0] for p in kp_norm if p[2] > 0.2]
             bbox_height = max(ys) - min(ys) if ys else 0
@@ -504,10 +504,8 @@ if uploaded_files:
 
             h, w = frame.shape[:2]
 
-
-            # annotated = draw_annotations(frame.copy(), rescaledkeypoints, punches, postures, gloves)
+            #annotated = draw_annotations(frame.copy(), rescaledkeypoints, punches, postures, gloves)
             annotated = draw_annotations(frame.copy(), rescaledkeypoints, punches, postures, gloves, h, w)
-
 
             out_writer.write(annotated)
 
