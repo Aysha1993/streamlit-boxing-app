2fcd652765f858ebd91c9c1cd246c77fbf393bf0
Aysha1993 <ayshuash95@gmail.com>
2025-06-06 07:14:57 +0000

    Update Streamlit boxing app

--- app.py
+++ app.py
@@ -258,9 +258,9 @@ def detect_gloves_by_color_and_shape(frame, keypoints, confidence_threshold=0.3,
     for person in keypoints:
         h, w, _ = frame.shape
 
-        if not is_punching_pose(person):
-            glove_detections.append({'left_glove': False, 'right_glove': False})
-            continue  # Skip glove check if not punching
+        # if not is_punching_pose(person):
+        #     glove_detections.append({'left_glove': False, 'right_glove': False})
+        #     continue  # Skip glove check if not punching
 
         def crop_wrist_region(wrist_index):
             kp = person[wrist_index]
@@ -361,9 +361,9 @@ def draw_annotations(frame, keypoints, punches, postures, glove_detections, h, w
     valid_detections = []
     for idx, (kp_raw, punch, posture, glovedetected) in enumerate(zip(keypoints, punches, postures, glove_detections)):
         person = kp_raw  # use the current person only
-        # if not is_punching_pose(person):
-        #     #st.info(f"Skipping Person {idx+1} - Not Punching")
-        #     continue
+        if not is_punching_pose(person):
+            #st.info(f"Skipping Person {idx+1} - Not Punching")
+            continue
 
         kp = np.array(kp_raw).reshape(-1, 3).tolist()
 
